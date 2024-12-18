6a1b63d904a08c01ff50706c13f0a219d9c512fa
Ibrahim <ibrahimisme5@gmail.com>
2024-12-18 17:00:11 +0500

    End

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: 'incremental',
+  },
 };
 
 export default nextConfig;
+
+module.exports = {
+  experimental: { 
+    turbo: false,
+  },
+};
